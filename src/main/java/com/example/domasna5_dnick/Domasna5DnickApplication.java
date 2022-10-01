98734ac9ce7f47a7388e1116134506adaf85de93
SretenS123 <strezovskisreten@gmail.com>
2022-10-01 03:35:18 +0200

    Project Version 2.0

--- src/main/java/com/example/domasna5_dnick/Domasna5DnickApplication.java
+++ src/main/java/com/example/domasna5_dnick/Domasna5DnickApplication.java
@@ -2,8 +2,22 @@ package com.example.domasna5_dnick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @SpringBootApplication
 public class Domasna5DnickApplication {
+    @Controller
+    @SpringBootApplication
+    public class DemoApplication {
+
+        @RequestMapping("/")
+        @ResponseBody
+        String home() {
+            return "Hello World!";
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Domasna5DnickApplication.class, args);
